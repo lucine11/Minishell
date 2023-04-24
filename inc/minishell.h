908148625c600d24e2c99a3956a2a16ddeae467e
Ucine Hamouni <62403196+UCINE@users.noreply.github.com>
2023-04-24 08:09:16 +0000

    Update minishell.h

--- inc/minishell.h
+++ inc/minishell.h
@@ -1,16 +1,20 @@
 #ifndef MINISHELL_H
-# define MINISHELL_H
+#define MINISHELL_H
 
+#include <stdbool.h>
 
-typedef struct			s_command 
-{
-	char				pipe;
-	char				type_in;
-	char				type_out;
-	char				*cmd;
-	int					fd_in;
-	int					fd_out;
-	int					return_value;
-	int					pipe_fd[2];
-}						t_command;
+#define MAX_ARGS 10
+
+typedef struct command {
+    char **args;
+    bool background;
+    char *input_file;
+    char *output_file;
+    bool append;
+    struct command *next;
+} command_t;
+
+command_t *parse_input(char *input);
+void free_command(command_t *cmd);
+
 #endif
