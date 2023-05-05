24df234feaa5cdb6af42166e6048941e8c5b6cba
Ucine Hamouni <62403196+UCINE@users.noreply.github.com>
2023-05-05 20:48:27 +0000

    update utils.c

--- src/utils.c
+++ src/utils.c
@@ -1,6 +1,7 @@
 #include "../inc/minishell.h"
 
-bool is_whitespace(char c) {
+bool is_whitespace(char c) 
+{
     return c == ' ' || c == '\t' || c == '\n' || c == '\r';
 }
 
@@ -8,7 +9,7 @@ t_token *new_token(char *value, bool is_quoted) {
     t_token *token = malloc(sizeof(t_token));
     if (!token) 
     {
-        write(2, "minishell: allocation error\n", 28);
+        printf("minishell: allocation error\n");
         exit(EXIT_FAILURE);
     }
     token->value = value;
