5b8f904b689e08ee010696cdb59be82182859bf7
Ucine Hamouni <62403196+UCINE@users.noreply.github.com>
2023-05-24 16:50:38 +0000

    get env

--- src/get_env_variables.c
+++ src/get_env_variables.c
@@ -6,11 +6,30 @@
 /*   By: lahamoun <lahamoun@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/24 16:24:28 by lahamoun          #+#    #+#             */
-/*   Updated: 2023/05/24 16:44:49 by lahamoun         ###   ########.fr       */
+/*   Updated: 2023/05/24 16:49:52 by lahamoun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/minishell.h"
+
+t_env	*ft_list_new(char *str)
+{
+	t_env	*new;
+
+	new = ft_calloc(sizeof(t_env), 1);
+	if (!new)
+		return (0);
+	if (!ft_strchr(str, '='))
+	{
+		new->name = ft_strdup(str);
+		if (!new->name)
+			return (0);
+		new->value = NULL;
+	}
+	else
+		create_and_assign(str, &new);
+	return (new);
+}
 
 t_env	*get_env_variables(char **envp)
 {
@@ -22,7 +41,7 @@ t_env	*get_env_variables(char **envp)
 	root = NULL;
 	while (envp[i])
 	{
-		new = ft_envlst_new(envp[i]);
+		new = ft_list_new(envp[i]);
 		ft_envlstadd_back(&root, new);
 		i++;
 	}
