7a76fbc08220db559994dd9130a2e556df4e5f13
Ucine Hamouni <62403196+UCINE@users.noreply.github.com>
2023-05-24 16:56:45 +0000

    get env

--- src/get_env_variables.c
+++ src/get_env_variables.c
@@ -6,11 +6,37 @@
 /*   By: lahamoun <lahamoun@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/24 16:24:28 by lahamoun          #+#    #+#             */
-/*   Updated: 2023/05/24 16:49:52 by lahamoun         ###   ########.fr       */
+/*   Updated: 2023/05/24 16:56:21 by lahamoun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/minishell.h"
+
+static void	create_and_assign(char *str, t_env **new)
+{
+	int	i;
+	int	var_value_size;
+
+	i = -1;
+	while (str[++i])
+	{
+		if (str[i] == '=')
+			break ;
+	}
+	if (str[i - 1] == '+')
+	{
+		(*new)->name = ft_calloc(sizeof(char), i);
+		ft_strlcpy((*new)->name, str, i);
+	}
+	else
+	{
+		(*new)->name = ft_calloc(sizeof(char), i + 1);
+		ft_strlcpy((*new)->name, str, i + 1);
+	}
+	var_value_size = ft_strlen(str) - ft_strlen((*new)->name);
+	(*new)->value = ft_calloc(sizeof(char), var_value_size);
+	ft_strlcpy((*new)->value, str + i + 1, var_value_size);
+}
 
 t_env	*ft_list_new(char *str)
 {
@@ -42,7 +68,7 @@ t_env	*get_env_variables(char **envp)
 	while (envp[i])
 	{
 		new = ft_list_new(envp[i]);
-		ft_envlstadd_back(&root, new);
+		ft_listadd_back(&root, new);
 		i++;
 	}
 	return (root);
