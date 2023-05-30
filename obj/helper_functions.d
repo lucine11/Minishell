5ee2cde360fc47c545f540e809cf6daec8205eda
Ucine Hamouni <62403196+UCINE@users.noreply.github.com>
2023-05-30 17:53:46 +0000

    add expand

--- obj/helper_functions.d
+++
@@ -1,2 +0,0 @@
-obj/helper_functions.o: src/helper_functions.c src/../inc/minishell.h \
- src/../inc/../libft/libft.h
--- obj/helper_unctionss.d
+++
@@ -1,2 +0,0 @@
-obj/helper_unctionss.o: src/helper_unctionss.c src/../inc/minishell.h \
- src/../inc/../libft/libft.h
