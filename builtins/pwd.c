03beb43512365b54eab4a16b067e1ba737edbe57
Ucine Hamouni <62403196+UCINE@users.noreply.github.com>
2023-06-01 18:05:16 +0000

    update bultins with exit status which stores ES of the last executed cmd

--- builtins/pwd.c
+++ builtins/pwd.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   pwd.c                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aelbouaa <aelbouaa@student.42.fr>          +#+  +:+       +#+        */
+/*   By: lahamoun < lahamoun@student.1337.ma>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/31 16:28:26 by aelbouaa          #+#    #+#             */
-/*   Updated: 2023/05/31 16:43:59 by aelbouaa         ###   ########.fr       */
+/*   Updated: 2023/06/01 17:58:20 by lahamoun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,11 @@ void    ft_pwd(void)
 {
     char *pwd;
 
+    exit_status = 0;
     pwd = getcwd(0, 0);
     if (!pwd)
         return ;
     printf("%s\n", pwd);
     free(pwd);
 }
+
