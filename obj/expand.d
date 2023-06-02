1fb8c88d350ca80a24f53da5742789b0ac9513f3
Ucine Hamouni <62403196+UCINE@users.noreply.github.com>
2023-06-02 18:56:15 +0000

    split commands to an array of array of strings

--- obj/expand.d
+++
@@ -1,2 +0,0 @@
-obj/expand.o: src/expand.c src/../inc/minishell.h \
- src/../inc/../libft/libft.h
--- obj/helper_function.d
+++
@@ -1,2 +0,0 @@
-obj/helper_function.o: src/helper_function.c src/../inc/minishell.h \
- src/../inc/../libft/libft.h
