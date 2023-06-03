3ac3b5ab455bd2e230cf07e8b6c961dc93b64511
Ucine Hamouni <62403196+UCINE@users.noreply.github.com>
2023-06-03 03:31:38 +0000

    handle redirections NOT WORKING YET

--- obj/heredoc_open.d
+++
@@ -1,2 +0,0 @@
-obj/heredoc_open.o: src/heredoc_open.c src/../inc/minishell.h \
- src/../inc/../libft/libft.h
--- obj/initialization.d
+++
@@ -1,2 +0,0 @@
-obj/initialization.o: src/initialization.c src/../inc/minishell.h \
- src/../inc/../libft/libft.h
